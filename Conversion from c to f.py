d81a70c5b828c56defa88497adca1c35ade3770d
uditdesai1 <83577440+uditdesai1@users.noreply.github.com>
2021-05-18 16:04:59 +0530

    Add files via upload
    This will convert Celsius to Fahrenheit and vice-versa.

---
+++ Conversion from c to f.py
@@ -0,0 +1,16 @@
+a = int(input('Enter the Temperature in °'))
+
+b = input('Enter the Temperature Scale \n(C for Celsius and F for Fahrenheit)\n')
+
+# Formula For Converting Celsius to Fahrenheit
+ctf = ((int(a)/5)*9)+32
+
+#  Formula for Converting Fahrenheit to Celsius
+ftc = ((int(a)-32)*5)/9
+
+
+
+if b == 'C':
+    print(str(a) + '° Celsius is ' + str(ctf) + '° Fahrenheit')
+elif b == 'F':
+    print(str(a) + '° Fahrenheit is ' + str(ftc) + '° Celsius')
